6c09488646ca3e1ec511321d51999e51e9560817
theDistantHours <3229466538@qq.com>
2021-04-14 18:19:13 +0800

    nothing

--- headers/commons.h
+++ headers/commons.h
@@ -3,3 +3,8 @@
 #include<string>
 #include<fstream>
 #include<ctime>
+
+class data {
+    virtual bool load(std::fstream file);
+    virtual bool write(std::fstream file);
+};
--- headers/stockman.h
+++ headers/stockman.h
@@ -5,78 +5,114 @@
 using namespace std;
 
 
-namespace StockMan {
-    //metadata structure definitions
-    enum userType {
-        guest, worker, admin
-    };
-    enum operationType {
-        add,remove,query,login
-    };
-    struct record;
-    struct userInfo;
-    struct stockAttr;
-    struct stockCategory;
-    struct stockItem;
-    struct loginToken;
-    class stockMan;
-
-    struct record {
-        time_t record_date;
-        operationType operation;
-        uid userid;
-        uid parameter;
-        string toString();
-    };
-
-    struct userInfo {
-        uid userid;
-        string username;
-        string password;
-        string description;
-        time_t regdate;
-
-        userType privilege;
-    };
-
-    struct stockAttr {
-        uid attr_id;
-        string attr_name;
-        string attr_desc;
-    };
-
-    struct stockCategory {
-        uid type_id;
-        string type_name;
-        string type_desc;
-        unsigned long long int durance;
-    };
-
-    struct stockItem {
-        uid id;
-        vector<stockCategory> types;
-        vector<stockAttr> attrs;
-        unsigned long long int count;
-        vector<unsigned long long int> durances;
-    };
-    
-    struct loginToken {
-        bool valid;
-        time_t login_time;
-        time_t logout_time;
-        userType usertype;
-        long userid;
-    };
 
 
-    class stockMan {
-    public:
-        loginToken login(string username, string password);
-        vector<stockCategory> &getCategories(void);
-        vector<stockAttr>& getAttributes(void);
-        bool addCategory(stockCategory type);
-        bool addAttr(stockAttr attr);
-    private:
-    };
-    
-}
+
+
+
+
+
+
+//metadata structure definitions
+enum userType {
+    guest, worker, admin
+};
+enum operationType {
+    add, remove, query, login
+};
+
+struct record;
+struct userInfo;
+struct stockAttr;
+struct stockCategory;
+struct stockItem;
+struct loginToken;
+class stockMan;
+
+
+
+
+
+
+
+
+
+
+
+
+struct record :public data {
+    time_t record_date;
+    operationType operation;
+    uid userid;
+    uid parameter;
+
+    bool write(fstream) override;
+    bool load(fstream) override;
+};
+
+struct userInfo :public data {
+    uid userid;
+    string username;
+    string password;
+    string description;
+    time_t regdate;
+
+    userType privilege;
+
+    bool write(fstream) override;
+    bool load(fstream) override;
+};
+
+struct stockAttr :public data {
+    uid attr_id;
+    string attr_name;
+    string attr_desc;
+
+    bool write(fstream) override;
+    bool load(fstream) override;
+};
+
+struct stockCategory :public data {
+    uid type_id;
+    string type_name;
+    string type_desc;
+    unsigned long long int durance;
+
+    bool write(fstream) override;
+    bool load(fstream) override;
+};
+
+struct stockItem :public data {
+    uid id;
+    vector<stockCategory> types;
+    vector<stockAttr> attrs;
+    unsigned long long int count;
+    vector<unsigned long long int> durances;
+
+    bool write(fstream) override;
+    bool load(fstream) override;
+};
+
+struct loginToken :public data {
+    bool valid;
+    time_t login_time;
+    time_t logout_time;
+    userType usertype;
+    long userid;
+
+    bool write(fstream) override;
+    bool load(fstream) override;
+};
+
+
+class stockMan {
+public:
+    loginToken login(string username, string password);
+    vector<stockCategory>& getCategories(void);
+    vector<stockAttr>& getAttributes(void);
+    bool addCategory(stockCategory type);
+    bool addAttr(stockAttr attr);
+private:
+};
+
+
